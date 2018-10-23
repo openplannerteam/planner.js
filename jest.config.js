fa707a140a60eb181049527c47eacb5d3bf860e1
Simon Vanneste <simon.vanneste@azumuta.com>
2018-10-23 15:16:53 +0200

    Added test for StopsFetcherNMBSLDFetch

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,5 @@ module.exports = {
     "json",
     "node"
   ],
-}
+  "setupTestFrameworkScriptFile": "<rootDir>/setup-jest.js"
+};
---
+++ src/fetcher/stops/StopsFetcherNMBSLDFetch.test.ts
@@ -0,0 +1,12 @@
+import "jest";
+import IStop from "./IStop";
+import IStopsFetcher from "./IStopsFetcher";
+import StopsFetcherNMBSLDFetch from "./StopsFetcherNMBSLDFetch";
+
+test("[StopsFetcherNMBSLDFetch]", async () => {
+  const fetcher: IStopsFetcher = new StopsFetcherNMBSLDFetch();
+  const stop: IStop = await fetcher.getStopById("http://irail.be/stations/NMBS/008896008");
+
+  expect(stop).toBeDefined();
+  expect(stop.name).toEqual("Kortrijk");
+});
