4d125ae2d4397021e2c7ecbb160d85a59eb38d4d
Pieter Colpaert <pieter.colpaert@ugent.be>
2018-11-15 15:55:47 +0100

    Added docs

--- src/Planner.ts
+++ src/Planner.ts
@@ -9,10 +9,17 @@ if (!Symbol.asyncIterator) {
   (Symbol as any).asyncIterator = Symbol.for("Symbol.asyncIterator");
 }
 
+/**
+ * Allows to ask route planning queries over our knowledge graphs
+ */
 export default class Planner {
   private context: Context;
   private queryRunner: IQueryRunner;
 
+  /**
+   * Initializes a new Planner
+   * @param container The container of dependencies we are working with
+   */
   constructor(container = defaultContainer) {
     // Store container on context before doing anything else
     this.context = container.get<Context>(TYPES.Context);
@@ -21,6 +28,12 @@ export default class Planner {
     this.queryRunner = container.get<IQueryRunner>(TYPES.QueryRunner);
   }
 
+  /**
+   * Given an [[IQuery]], it will evaluate the query and eventually produce an [[IQueryResult]]
+   * @todo Should return an AsyncIterator
+   * @param query 
+   * @returns A promise of an [[IQueryResult]]
+   */
   public async query(query: IQuery): Promise<IQueryResult> {
     return this.queryRunner.run(query);
   }
