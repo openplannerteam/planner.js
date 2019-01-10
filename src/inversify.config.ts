8598e2c4fd5d0f4ff18fde3b9343216da7ebef51
Maxim Martin <maxim@azumuta.com>
2019-01-10 14:17:49 +0100

    1. added ReachableStopsFinderOnlySelf. 2. cleanup JourneyExtraction phases.

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -14,13 +14,13 @@ import StopsFetcherLDFetch from "./fetcher/stops/ld-fetch/StopsFetcherLDFetch";
 import StopsProviderDefault from "./fetcher/stops/StopsProviderDefault";
 import IJourneyExtractor from "./planner/public-transport/IJourneyExtractor";
 import IPublicTransportPlanner from "./planner/public-transport/IPublicTransportPlanner";
-import JourneyExtractionPhase from "./planner/public-transport/JourneyExtractionPhase";
 import JourneyExtractorDefault from "./planner/public-transport/JourneyExtractorDefault";
 import PublicTransportPlannerCSAProfile from "./planner/public-transport/PublicTransportPlannerCSAProfile";
 import IRoadPlanner from "./planner/road/IRoadPlanner";
 import RoadPlannerBirdsEye from "./planner/road/RoadPlannerBirdsEye";
 import IReachableStopsFinder from "./planner/stops/IReachableStopsFinder";
-import ReachableStopsFinderBirdsEyeCached from "./planner/stops/ReachableStopsFinderBirdsEyeCached";
+import ReachableStopsFinderOnlySelf from "./planner/stops/ReachableStopsFinderOnlySelf";
+import ReachableStopsFinderRoadPlannerCached from "./planner/stops/ReachableStopsFinderRoadPlannerCached";
 import ReachableStopsSearchPhase from "./planner/stops/ReachableStopsSearchPhase";
 import QueryRunnerExponential from "./query-runner/exponential/QueryRunnerExponential";
 import ILocationResolver from "./query-runner/ILocationResolver";
@@ -39,21 +39,18 @@ container.bind<IPublicTransportPlanner>(TYPES.PublicTransportPlanner)
 container.bind<interfaces.Factory<IPublicTransportPlanner>>(TYPES.PublicTransportPlannerFactory)
   .toAutoFactory<IPublicTransportPlanner>(TYPES.PublicTransportPlanner);
 
+container.bind<IRoadPlanner>(TYPES.RoadPlanner)
+  .to(RoadPlannerBirdsEye);
+
 container.bind<IJourneyExtractor>(TYPES.JourneyExtractor)
   .to(JourneyExtractorDefault);
-container.bind<IRoadPlanner>(TYPES.RoadPlanner)
-  .to(RoadPlannerBirdsEye).whenTargetTagged("phase", JourneyExtractionPhase.Initial);
-container.bind<IRoadPlanner>(TYPES.RoadPlanner)
-  .to(RoadPlannerBirdsEye).whenTargetTagged("phase", JourneyExtractionPhase.Transfer);
-container.bind<IRoadPlanner>(TYPES.RoadPlanner)
-  .to(RoadPlannerBirdsEye).whenTargetTagged("phase", JourneyExtractionPhase.Final);
 
 container.bind<IReachableStopsFinder>(TYPES.ReachableStopsFinder)
-  .to(ReachableStopsFinderBirdsEyeCached).whenTargetTagged("phase", ReachableStopsSearchPhase.Initial);
+  .to(ReachableStopsFinderRoadPlannerCached).whenTargetTagged("phase", ReachableStopsSearchPhase.Initial);
 container.bind<IReachableStopsFinder>(TYPES.ReachableStopsFinder)
-  .to(ReachableStopsFinderBirdsEyeCached).whenTargetTagged("phase", ReachableStopsSearchPhase.Transfer);
+  .to(ReachableStopsFinderOnlySelf).whenTargetTagged("phase", ReachableStopsSearchPhase.Transfer);
 container.bind<IReachableStopsFinder>(TYPES.ReachableStopsFinder)
-  .to(ReachableStopsFinderBirdsEyeCached).whenTargetTagged("phase", ReachableStopsSearchPhase.Final);
+  .to(ReachableStopsFinderRoadPlannerCached).whenTargetTagged("phase", ReachableStopsSearchPhase.Final);
 
 container.bind<IConnectionsProvider>(TYPES.ConnectionsProvider).to(ConnectionsProviderMerge).inSingletonScope();
 container.bind<IConnectionsFetcher>(TYPES.ConnectionsFetcher).to(ConnectionsFetcherLazy);
--- src/planner/PlannerPhase.ts
+++
@@ -1,6 +0,0 @@
-import JourneyExtractionPhase from "./public-transport/JourneyExtractionPhase";
-import ReachableStopsSearchPhase from "./stops/ReachableStopsSearchPhase";
-
-type PlannerPhase = JourneyExtractionPhase | ReachableStopsSearchPhase;
-
-export default PlannerPhase;
