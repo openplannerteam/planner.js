4f497797ada7c668ea724e2cd6114480237f408a
Simon Vanneste <simon.vanneste@azumuta.com>
2019-01-24 11:55:30 +0100

    Reduce bundle size by excluding ldfetch dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require("path");
 
+// These modules are imported by ldfetch, but are never actually used because (right now) we only fetch jsonld files
+const excludeModules = [
+  "rdfa-processor",
+  "rdf-canonize",
+  "rdfxmlprocessor",
+  "xmldom",
+  'n3'
+];
+
 module.exports = {
   entry: "./src/index.ts",
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   module: {
     rules: [
       {
@@ -13,13 +22,17 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: [".ts", ".js"],
+    alias: excludeModules.reduce((alias, moduleName) => {
+      alias[moduleName] = path.resolve(__dirname, "webpack.mockModule.js");
+      return alias;
+    }, {})
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist'),
-    library: 'Planner',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
+    path: path.resolve(__dirname, "dist"),
+    library: "Planner",
+    libraryTarget: "umd",
+    libraryExport: "default"
   }
 };
