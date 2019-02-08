42d365afe4847b079bfea95d049b66699dadc3da
Simon Vanneste <simon.vanneste@azumuta.com>
2019-02-08 09:57:22 +0100

    Restored some things lost by merge

--- src/query-runner/LocationResolverDefault.ts
+++ src/query-runner/LocationResolverDefault.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "inversify";
+import LocationResolverError from "../errors/LocationResolverError";
 import IStop from "../fetcher/stops/IStop";
 import IStopsProvider from "../fetcher/stops/IStopsProvider";
 import ILocation from "../interfaces/ILocation";
@@ -30,7 +31,7 @@ export default class LocationResolverDefault implements ILocationResolver {
         return this.resolveById(input);
       }
 
-      return Promise.reject(`Location "${input}" is a string, but not an ID`);
+      return Promise.reject(new LocationResolverError(`Location "${input}" is a string, but not an ID`));
     }
 
     const location: ILocation = input;
@@ -44,7 +45,9 @@ export default class LocationResolverDefault implements ILocationResolver {
     }
 
     if (!hasCoords) {
-      return Promise.reject(`Location "${JSON.stringify(input)}" should have latitude and longitude`);
+      return Promise.reject(
+        new LocationResolverError(`Location "${JSON.stringify(input)}" should have latitude and longitude`),
+      );
     }
 
     return location;
@@ -62,7 +65,7 @@ export default class LocationResolverDefault implements ILocationResolver {
       };
     }
 
-    throw new Error(`No fetcher for id ${id}`);
+    return Promise.reject(new LocationResolverError(`No fetcher for id ${id}`));
   }
 
   private isId(testString: string): boolean {
