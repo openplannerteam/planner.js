9ea76a16d20cafdbb0df2a611668eb3882328ab3
Maxim Martin <maxim@azumuta.com>
2019-02-11 11:02:30 +0100

    csa and QueryRunnerEarliestArrivalFirst docs

--- src/query-runner/earliest-arrival-first/LinearQueryIterator.ts
+++ src/query-runner/earliest-arrival-first/LinearQueryIterator.ts
@@ -2,7 +2,9 @@ import { AsyncIterator } from "asynciterator";
 import { DurationMs } from "../../interfaces/units";
 import IResolvedQuery from "../IResolvedQuery";
 
-// Inspired by IntegerIterator
+/**
+ * This AsyncIterator emits [[IResolvedQuery]] instances with linear increasing `maximumArrivalTime`.
+ */
 export default class LinearQueryIterator extends AsyncIterator<IResolvedQuery> {
   private readonly baseQuery: IResolvedQuery;
   private timespan: DurationMs;
