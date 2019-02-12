800acaea3eb48d97bfea3a4b4a53836a86d128f5
Simon Vanneste <simon.vanneste@azumuta.com>
2019-02-12 10:24:21 +0100

    Change doc config to include asynciterator declarations

--- typedoc.config.js
+++ typedoc.config.js
@@ -11,7 +11,7 @@ module.exports = {
   mode: 'file',
   excludePrivate: true,
   excludeNotExported: false,
-  excludeExternals: false,
-  includeDeclarations: false,
+  excludeExternals: true,
+  includeDeclarations: true,
   theme: 'minimal',
 };
