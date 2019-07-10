e0f7ac5acd77e841299e1cb1962736db14c5d3f4
Harm Delva <harm.delva@gmail.com>
2019-07-10 14:55:30 +0200

    Remove more ded stuff

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,6 @@ import "reflect-metadata";
 import IsochroneGenerator from "./analytics/isochrones/main";
 import Planner from "./Planner";
 
-/*
-import defaultContainer from "./inversify.config";
-import Context from "./Context";
-import TYPES from "./types";
-import IProfileFetcher from "./fetcher/profiles/IProfileFetcher";
-
-const x = new IsochroneGenerator();
-x.init({latitude: 51.0262973, longitude: 3.7110885}).then(async () => {
-    const i = await x.getIsochrone(10000);
-    let y = 9;
-})
-*/
-
 export default {
     Planner,
     IsochroneGenerator,
