9f91e4dfd75eddb3161aea61c1e07eecdad0f983
Harm Delva <harm.delva@gmail.com>
2019-07-16 16:14:33 +0200

    Remove test code

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,6 @@ import "reflect-metadata";
 import IsochroneGenerator from "./analytics/isochrones/main";
 import Planner from "./Planner";
 
-/*
-async function dostuff() {
-    const x = new IsochroneGenerator({ latitude: 51.03863, longitude: 3.72634 });
-    x.setProfileID("PEDESTRIAN");
-    let a = await x.getIsochrone(50000, true);
-    a = await x.getIsochrone(150000, false);
-    x.setProfileID("http://hdelva.be/profile/car");
-    a = await x.getIsochrone(150000, true);
-}
-
-dostuff().then(() => {
-    let a = 9;
-})
-*/
-
 export default {
     Planner,
     IsochroneGenerator,
