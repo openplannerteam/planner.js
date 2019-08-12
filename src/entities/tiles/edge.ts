77d0e360fc0b2cabb93670c403cc21ab2105df70
Harm Delva <harm.delva@gmail.com>
2019-08-12 21:06:12 +0200

    Commit missing files

---
+++ src/entities/tiles/edge.ts
@@ -0,0 +1,5 @@
+export default class Edge {
+    public from: string;
+    public to: string;
+    public distance?: number;
+}
---
+++ src/fetcher/tiles/RoutableTileProviderTransit.ts
@@ -0,0 +1,21 @@
+import { inject, injectable } from "inversify";
+import { RoutableTileCoordinate } from "../../entities/tiles/coordinate";
+import RoutableTileRegistry from "../../entities/tiles/registry";
+import TYPES from "../../types";
+import IRoutableTileFetcher from "./IRoutableTileFetcher";
+import RoutableTileProviderDefault from "./RoutableTileProviderDefault";
+
+@injectable()
+export default class RoutableTileProviderTransit extends RoutableTileProviderDefault {
+
+  constructor(
+    @inject(TYPES.RoutableTileFetcher) fetcher: IRoutableTileFetcher,
+    @inject(TYPES.RoutableTileRegistry) registry: RoutableTileRegistry,
+  ) {
+      super(fetcher, registry);
+  }
+
+  public getIdForTileCoords(coordinate: RoutableTileCoordinate): string {
+    return `http://hdelva.be/tiles/reduced/${coordinate.zoom}/${coordinate.x}/${coordinate.y}`;
+  }
+}
