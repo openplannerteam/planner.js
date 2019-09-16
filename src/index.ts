e186d3bd53de7fc852e12fdecb663335eaa86e8b
Harm Delva <harm.delva@gmail.com>
2019-09-16 17:26:44 +0200

    Bump version number

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import "isomorphic-fetch";
 import "reflect-metadata";
 import getEventBus from "./events/util";
 
-export {default as EventType} from "./events/EventType";
-export {default as IsochroneGenerator} from "./analytics/isochrones/main";
-export {default as Planner} from "./Planner";
-export {default as Units} from "./util/Units";
+export { default as EventType } from "./events/EventType";
+export { default as IsochroneGenerator } from "./analytics/isochrones/main";
+export { default as Planner } from "./Planner";
+export { default as Units } from "./util/Units";
 
 export const EventBus = getEventBus();
