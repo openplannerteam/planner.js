a8bdda3e538116213dbd526dcc758ce5cd4a2800
Harm Delva <harm.delva@gmail.com>
2019-12-19 16:33:09 +0100

    Fix connections selectors

---
+++ src/fetcher/connections/ConnectionSelectors.ts
@@ -0,0 +1,41 @@
+import IConnection from "../../entities/connections/connections";
+
+export function forwardsConnectionSelector(connections: IConnection[]): number {
+    if (connections.length === 1) {
+        return 0;
+    }
+
+    let earliestIndex = 0;
+    let earliest = connections[earliestIndex];
+
+    for (let i = 1; i < connections.length; i++) {
+        const connection = connections[i];
+
+        if (connection.departureTime < earliest.departureTime) {
+            earliestIndex = i;
+            earliest = connection;
+        }
+    }
+
+    return earliestIndex;
+}
+
+export function backwardsConnectionsSelector(connections: IConnection[]): number {
+    if (connections.length === 1) {
+        return 0;
+    }
+
+    let latestIndex = 0;
+    let latest = connections[latestIndex];
+
+    for (let i = 1; i < connections.length; i++) {
+        const connection = connections[i];
+
+        if (connection.departureTime > latest.departureTime) {
+            latestIndex = i;
+            latest = connection;
+        }
+    }
+
+    return latestIndex;
+}
